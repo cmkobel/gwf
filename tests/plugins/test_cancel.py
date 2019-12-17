c833625c8fea340e916729f5a9cfcafb77cd125b
Dan Søndergaard <das@birc.au.dk>
2019-12-17 15:22:39 +0100

    Fix tests for cancel and clean plugins

--- tests/plugins/test_cancel.py
+++ tests/plugins/test_cancel.py
@@ -18,8 +18,18 @@ def simple_workflow():
 
 
 def test_cancel_one_target(cli_runner):
-    result = cli_runner.invoke(main, ["-b", "testing", "cancel", "Target1"])
-    assert result.output == "Cancelling target Target1.\n"
+    result = cli_runner.invoke(
+        main,
+        [
+            "-b",
+            "testing",
+            "-f",
+            "examples/simple-workflow/workflow.py",
+            "cancel",
+            "Target1",
+        ],
+    )
+    assert result.output == "Cancelling target Target1\n"
 
 
 def test_cancel_two_targets(cli_runner):
